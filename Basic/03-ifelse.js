8dbc6da75ee404ca63b4ccc089ec6d3d381764f7
Parmjeet Mishra <parmjeetmishra2@gmail.com>
2023-07-15 17:31:44 +0530

    JS Repo For All JS code

---
+++ Basic/03-ifelse.js
@@ -0,0 +1,28 @@
+let user = 'teacher';
+
+if (user === 'student') {
+    console.log('student')
+}
+else if (user === 'teacher') {
+    console.log('teacher');
+} else {
+    console.log('unknown');
+}
+
+console.log('Grading');
+
+let marks = 88;
+
+if (marks < 33) {
+    console.log('Fail');
+} else if (marks === 33) {
+    console.log('Barely Pass');
+} else if (marks === 60) {
+    console.log('Pass-It could be better');
+} else if (marks === 80) {
+    console.log('Excellent')
+} else if (marks === 90) {
+    console.log('Very Good');
+} else {
+    console.log('Please enter your marks');
+}
