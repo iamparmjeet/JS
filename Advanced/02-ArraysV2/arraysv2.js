9994c05d7a632f2c35109f7c66e8f190f1a01650
Parmjeet Mishra <parmjeetmishra2@gmail.com>
2023-07-21 18:10:32 +0530

    Arrays Update

--- Advanced/02-ArraysV2/arraysv2.js
+++ Advanced/02-ArraysV2/arraysv2.js
@@ -5,7 +5,7 @@ let dcHeros = ['superman', 'batman', 'flash']
 marvelHeros.push(dcHeros) // It did not merge but add as a array
 
 console.log(marvelHeros)
-console.log(marvelHeros)
+console.log(marvelHeros) 
 
 marvelHeros.concat(dcHeros)
 
