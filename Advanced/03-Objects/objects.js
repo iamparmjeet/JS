d76eb464daf8bb7120ed9a2305d71c7dc2fc2bda
Parmjeet Mishra <parmjeetmishra2@gmail.com>
2023-07-22 14:51:18 +0530

    Objects

---
+++ Advanced/03-Objects/objects.js
@@ -0,0 +1,6 @@
+// Declaring objects - 1) Literals 2) Constructors
+// Singleton -> Unique objects from Constructors -> Objects.create
+// Multiple Instances When from Literals -> {}
+
+console.log('Object literals')  // keys and Values
+
