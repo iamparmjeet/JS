c6b748f745985c22569fcdd88f6166a1fffc157b
Parmjeet Mishra <parmjeetmishra2@gmail.com>
2023-07-25 14:17:36 +0530

    Loop

---
+++ Advanced/08-Control Flow/02-loop.js
@@ -0,0 +1,35 @@
+// Loop -- Iterations
+
+for (let index = 0; index <= 10; index++) {
+    const element = index
+    //console.log(element)
+}
+
+// for (let i = 0; i <= 2; i++) {
+//     console.log(`Outer Loop ${i}`)
+//     for (let j = 0; j <= 2; j++) {
+//         console.log(`Inner Loop ${j} and outer Loop ${i}`)
+//     }
+// }
+
+console.log('Table with loop')
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Table of  ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+// }
+
+let heroArray = ['flash', 'Shaktiman', 'Superman', 'Captain Marvel']
+
+for (let i = 0; i < heroArray.length; i++) {
+    //console.log(heroArray[i])
+}
+
+// Break and Continue
+console.log('BREAK AND CONTINUE')
+
+for (let i = 0; i <= 20; i++) {
+    
+}
