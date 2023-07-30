326db40e39313979734a025311fb714c76b91c76
Parmjeet Mishra <parmjeetmishra2@gmail.com>
2023-07-30 13:53:19 +0530

    While Loop

---
+++ Advanced/08-Control Flow/03-whileloop.js
@@ -0,0 +1,26 @@
+
+//let index = 0
+// while (index <= 10) {
+//     console.log(`Index of ${index}`)
+//     index = index + 2
+// }
+
+// LOOPS
+
+// let superArray = ['flash', 'Superman', 'Shaktiman']
+
+// let arr = 0
+
+// while (arr < superArray.length) {
+//     console.log(superArray[arr])
+//     arr = arr + 1
+// }
+
+// DO While Loop -> First value check then work and atleast work single time
+
+let score = 11
+
+do {
+    console.log(score)
+    score++
+} while (score <= 10);
