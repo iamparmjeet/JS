9d612d80b097280864e48905e18f76050875d7f1
Parmjeet Mishra <parmjeetmishra2@gmail.com>
2023-07-31 23:37:41 +0530

    JS Mini project - 02-ColorChanger Pending

---
+++ JS-Projects(Mini)/P2-ColorChanger/color.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll('.colorBox');
+console.log(buttons)
+let body = document.querySelector('body')
+
+//Adding Event Listner to Every Button
+
+buttons.forEach(function (button) {
+    // console.log(buttons)
+} )
