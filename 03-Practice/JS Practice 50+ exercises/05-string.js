fbe686e113d6cd79f705964c8b3b1c3060da9f21
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-11 00:22:18 +0530

    Practice

---
+++ 03-Practice/JS Practice 50+ exercises/05-string.js
@@ -0,0 +1,8 @@
+let str = 'Hello world'
+
+function revString(str) {
+   value = str.split('').reverse().join('-')
+   return value
+}
+
+revString(str)
---
+++ 03-Practice/JS Practice 50+ exercises/06-oddAndEven.js
@@ -0,0 +1,10 @@
+
+function checkOddOrEven(num) {
+   if (num % 2 === 0) {
+      return 'even'
+   } else return 'odd'
+}
+
+let res = checkOddOrEven(7)
+
+console.log(res)
