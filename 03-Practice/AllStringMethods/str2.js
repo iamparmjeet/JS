ad567567aa17fbc94b3c023d1a12531a87d1e069
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-22 23:49:01 +0530

    All Strings Methods-> 06 String Split

--- 03-Practice/AllStringMethods/str2.js
+++ 03-Practice/AllStringMethods/str2.js
@@ -1,3 +1,37 @@
-const str = 'The morning is upon us.'
+const str = "The morning is upon us.";
 
-console.log('06 SPLIT')
+console.log("06 SPLIT");
+// Does not modify the orignal array
+// Returns array
+/*
+
+str.split()
+str.split(seperator)
+str.split(seperator, limit)
+
+*/
+
+
+console.log(str);
+console.log(str.split());
+
+let arr1 = str.split();
+console.log(arr1);
+console.log(typeof arr1); // object
+
+console.log(str.split(" ")); // [ 'The', 'morning', 'is', 'upon', 'us.' ]
+console.log(str.split(""));
+/*
+[
+  'T', 'h', 'e', ' ', 'm',
+  'o', 'r', 'n', 'i', 'n',
+  'g', ' ', 'i', 's', ' ',
+  'u', 'p', 'o', 'n', ' ',
+  'u', 's', '.'
+]
+*/
+
+console.log(str.split('o')) // [ 'The m', 'rning is up', 'n us.' ]
+
+console.log(str)
+console.log(str.split('o', 2)) // [ 'The m', 'rning is up' ]
