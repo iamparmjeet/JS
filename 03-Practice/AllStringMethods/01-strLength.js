93fa16add6f0311a176b9debfa55b801f7530b75
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-24 13:14:29 +0530

    All Strings Methods-> 01 Str Length

---
+++ 03-Practice/AllStringMethods/01-strLength.js
@@ -0,0 +1,9 @@
+
+const str = 'The morning is upon us.'
+
+// 01-String Properties - Length
+console.log('Str length')
+console.log(str.length)
+
+console.log('Character at')
+console.log(str.charAt(5))
