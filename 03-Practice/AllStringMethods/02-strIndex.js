49714c80443ba779d797cd8e60f4d0b292b6ff49
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-24 13:15:21 +0530

    All Strings Methods-> 02 IndexOf and LastIndexOf

---
+++ 03-Practice/AllStringMethods/02-strIndex.js
@@ -0,0 +1,9 @@
+// 02 Index of
+console.log('Index of')
+// str.indexOf(searchStr, position)
+console.log(str.indexOf('JS'))
+
+// 03 last Indexof
+// str.lastIndexOf(searchStr, position)
+console.log('Last Indexof')
+console.log(str.lastIndexOf('Code'))
