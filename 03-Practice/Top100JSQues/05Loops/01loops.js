ffee7c8d575c129a5c90eccc58cd9fed4c97d992
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-26 23:39:50 +0530

    Top100JSQues->Loop

---
+++ 03-Practice/Top100JSQues/05Loops/01loops.js
@@ -0,0 +1,26 @@
+// What is a loop, what are the types of loops in JS
+
+// for-loop
+
+
+for (let i = 0; i < 5; i++) {
+   console.log(i)
+}
+
+// While loop
+
+console.log('while loop')
+
+let j = 6
+
+while (j < 5) {
+   console.log(j)
+   j++
+}
+
+// without condition (infinity)
+
+// while ('a' == 'a') {
+//    console.log('Happy')
+// }
+
