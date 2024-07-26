65e0c5eefcfbe6373921d01a33fa9728a44da8c1
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-26 23:40:22 +0530

    Top100JSQues->while loop

---
+++ 03-Practice/Top100JSQues/05Loops/02while.js
@@ -0,0 +1,20 @@
+// do-while
+
+let i = 0
+
+// while loop
+console.log('while')
+
+while (i < 5) {
+   console.log(i)
+   i++
+}
+
+
+console.log('do-while')
+
+do {
+   console.log(i)
+   i++
+   break;
+} while (i > 5);
