20552549ff9cbeb04847e63f71b66f783a71b684
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-27 23:22:49 +0530

    Top100JSQues->06Functions-function expression

---
+++ 03-Practice/Top100JSQues/06Functions/03funcExpression.js
@@ -0,0 +1,15 @@
+// Anonymous function expression
+
+const add = function (a, b) {
+   return a + b
+}
+
+console.log(add(5, 3))
+
+// Named function expression
+
+const add1 = function sum(a, b) {
+   return a + b
+}
+
+console.log(add1(3,3))
