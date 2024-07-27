12ae08e62c8f38df188eab6483e012ae0be92314
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-27 23:24:18 +0530

    Top100JSQues->06Functions-Arguments and parameters of function

---
+++ 03-Practice/Top100JSQues/06Functions/06Arg&param.js
@@ -0,0 +1,9 @@
+// Parameters are the placeholders defined in the function declaration
+
+// a and b - parameters
+
+function add(a, b) {
+   console.log(a + b)
+}
+
+add(3,4) // Here 3 and 4 are arguments
