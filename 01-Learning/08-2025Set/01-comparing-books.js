ad499af1e6a48cfd71f4c82ad91302c0d7fad372
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-18 14:35:27 +0530

    Update=>New Set function in JS 2025

---
+++ 01-Learning/08-2025Set/01-comparing-books.js
@@ -0,0 +1,16 @@
+const fantasyBooksArr = [
+  "Harry Potter",
+  "The Hobbit",
+  "The Lord of the Rings", // Overlap
+  "A Song of Ice and Fire", // Overlap
+];
+
+const actionBooksArr = [
+  "The Hunger Games",
+  "Divergent",
+  "The Maze Runner",
+  "The Lord of the Rings", // Overlap
+  "A Song of Ice and Fire", // Overlap
+];
+
+const fantasyBooksSet = new Set(fantasyBooksArr);
