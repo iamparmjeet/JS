ad21328381d7f5e64479e1a7cf02c41ee30ef633
parmjeet <iamparmjeetmishra@gmail.com>
2025-09-18 15:29:16 +0530

    Update=>New Set function and benchmarking using deno in JS 2025

---
+++ 04-Benchmarking/01-Set/benchmarking.js
@@ -0,0 +1,76 @@
+import { faker } from "@faker-js/faker";
+import * as compare from "./01-comparing-books.js";
+
+/* ------------- Make Fake Data ------------- */
+
+// make an array of 1,000 fantasy books
+const fantasyBooksArray = [];
+for (let i = 0; i < 1000; i++) {
+  fantasyBooksArray.push(faker.lorem.words());
+}
+
+// make an array of 1,000 action books
+const actionBooksArray = [];
+for (let i = 0; i < 1000; i++) {
+  actionBooksArray.push(faker.lorem.words());
+}
+
+const fantasyBooksSet = new Set(fantasyBooksArray);
+const actionBooksSet = new Set(actionBooksArray);
+
+console.log("actionBooksArray: ", actionBooksArray.length);
+console.log("fantasyBooksArray: ", fantasyBooksArray.length);
+console.log("fantasyBooksSet: ", fantasyBooksSet.size);
+console.log("actionBooksSet: ", actionBooksSet.size);
+
+/* ------------- Run Benchmarks ------------------ */
+
+Deno.bench("diff arr:", () => {
+  compare.diffArr(fantasyBooksArray, actionBooksArray);
+});
+
+Deno.bench("diff set:", () => {
+  compare.diffSet(fantasyBooksSet, actionBooksSet);
+});
+
+Deno.bench("intersection arr:", () => {
+  compare.intersectionArr(fantasyBooksArray, actionBooksArray);
+});
+Deno.bench("intersection set:", () => {
+  compare.intersectionSet(fantasyBooksSet, actionBooksSet);
+});
+
+Deno.bench("symDiff arr:", () => {
+  compare.symDiffArr(fantasyBooksArray, actionBooksArray);
+});
+Deno.bench("symDiff set:", () => {
+  compare.symDiffSet(fantasyBooksSet, actionBooksSet);
+});
+
+Deno.bench("union arr:", () => {
+  compare.unionArr(fantasyBooksArray, actionBooksArray);
+});
+Deno.bench("union set:", () => {
+  compare.unionSet(fantasyBooksSet, actionBooksSet);
+});
+
+Deno.bench("isDisjointFrom arr:", () => {
+  compare.isDisjointFromArr(fantasyBooksArray, actionBooksArray);
+});
+Deno.bench("isDisjointFrom set:", () => {
+  compare.isDisjointFromSet(fantasyBooksSet, actionBooksSet);
+});
+
+Deno.bench("isSubsetOf arr:", () => {
+  compare.isSubsetOfArr(fantasyBooksArray, actionBooksArray);
+});
+Deno.bench("isSubsetOf set:", () => {
+  compare.isSubsetOfSet(fantasyBooksSet, actionBooksSet);
+});
+
+Deno.bench("isSupersetOf arr:", () => {
+  compare.isSupersetOfArr(fantasyBooksArray, actionBooksArray);
+});
+Deno.bench("isSupersetOf set:", () => {
+  compare.isSupersetOfSet(fantasyBooksSet, actionBooksSet);
+});
